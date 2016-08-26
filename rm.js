587a1b96ab0b44e6b7a80a2b091f7cb8c79f64af
rhapsodyn <rhapsodyn@gmail.com>
2016-08-26 21:11:59 +0800

    local tested & worked

---
+++ rm.js
@@ -0,0 +1,25 @@
+var nodes = document.getElementsByClassName('diff-file file-holder'),
+	rmCount = 0,
+	evilWords = ['.meta/diff'];
+
+function isNonsensePath(path) {
+	for (var i = 0; i < evilWords.length; i++) {
+		if (path.endsWith(evilWords[i])) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+for (var i = 0; i < nodes.length; i++) {
+	var node = nodes[i];
+	var path = node.dataset["blobDiffPath"];
+
+	if (isNonsensePath(path)) {
+		node.parentElement.removeChild(node);
+		rmCount++;
+	}
+}
+
+console.log("killed: " + rmCount + " diff doms");
